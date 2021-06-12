9902ba89015c0b14fcab4cc6cc16f8f85f73607b
wj <jannis-wilkening@gmx.de>
2021-06-12 22:47:07 +0200

    Fix eh_frame

--- tests/elfs/strip_elf.sh
+++ tests/elfs/strip_elf.sh
@@ -2,6 +2,6 @@
 
 llvm-objcopy-12 --remove-section .rel.debug_line test_simple_add_gdb.o
 llvm-objcopy-12 --remove-section .rel.debug_info  test_simple_add_gdb.o
-llvm-objcopy-12 --remove-section .rel.debug_info  test_simple_add_gdb.o
+llvm-objcopy-12 --remove-section .rel.eh_frame  test_simple_add_gdb.o
 llvm-objcopy-12 --remove-section .eh_frame test_simple_add_gdb.o
 llvm-objcopy-12 --remove-section .rel.BTF.ext test_simple_add_gdb.o
